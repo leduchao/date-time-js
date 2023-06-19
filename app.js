86c1913297a9b58dfa42ffbd6882e5fd9e39ad8e
leduchao <leduchao77g1@gmail.com>
2023-06-19 23:56:12 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+setInterval(() => {
+    let time = new Date();
+
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
+    let dayOrNight = 'AM';
+
+    if (hours > 12) {
+        hours -= 12;
+        dayOrNight = 'PM';
+    }
+
+    if (hours == 0) {
+        hours = 12;
+        dayOrNight = 'AM';
+    }
+
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    const option = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric'
+    }
+
+    let today = time.toLocaleDateString('en-US', option);
+
+    let timeText = document.querySelector('#time');
+    let dateText = document.querySelector('#date');
+    
+    timeText.textContent = hours + ' : ' + minutes + ' : ' + seconds + ' ' + dayOrNight;
+    dateText.textContent = today; //days + ' / ' + months + ' / ' + years;
+}, 1000);
+
